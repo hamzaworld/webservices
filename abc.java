9ba3b5583ed85893e478926bc60fdc712a4e7584
Hamza Akhtar <hamza019akhtar@gmail.com>
2020-11-30 20:56:45 +0530

    Add files via upload

---
+++ abc.java
@@ -0,0 +1,11 @@
+class abc{
+public static void main(String... args){
+
+int i1 = Integer.parseInt("hamza"); //1.4
+
+//double i2 = Double.parseDouble(args[1]);
+
+//System.out.println(i1+i2);
+}
+
+}
